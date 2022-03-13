b4f209fba39de327e49654272a00176176430e30
Evilbunny <kmalkin@hotmail.co.uk>
2022-03-13 17:24:44 +0000

    Feature/mc 002 (#2)
    * MC-001: updated some vars some plugins and tools and started to add the spot fleet asg
    
    * MC-001: updated some vars some plugins and tools and started to add the spot fleet asg
    
    * MC-002: updated deprecated arguments , updated logical flow, updated variables and params
    
    * MC-002: updated format and standards
    
    * MC-002: updated vars and ami lookup and formatting and have now completed testing
    
    * MC-002: updated vars and ami lookup and formatting and tested
    
    * MC-002: version updated
    
    * MC-0002: updated changelog
    
    Co-authored-by: Karl Malkin <kmalkin@williamhill.co.uk>

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -1,29 +1,48 @@
-resource "aws_security_group" "security_group" {
-  name        = "${var.prefix_identifier}security-group"
-  description = "Configures All rules required for ${var.prefix_identifier}server instance access and management"
+resource "aws_security_group" "server_security_group" {
+  name        = "${var.prefix_identifier}-security-group"
+  description = "Configures All rules required for ${var.prefix_identifier}-server instance access and management"
 
   ingress {
     description = "Home LAN SSH Access"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${var.ingress_access_ip_address}/32"]
+    cidr_blocks = ["${var.ingress_access_cidr}"]
   }
 
   ingress {
-    description = "Open Ingress on Minecraft Server Port"
+    description = "Home LAN MCRCON Access"
+    from_port   = 25575
+    to_port     = 25575
+    protocol    = "tcp"
+    cidr_blocks = ["${var.ingress_access_cidr}"]
+  }
+
+  #tfsec:ignore:aws-vpc-no-public-ingress-sgr
+  ingress {
+    description = "Minecraft Server Rule #1"
     from_port   = 25565
     to_port     = 25565
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS008
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  #tfsec:ignore:aws-vpc-no-public-ingress-sgr
+  ingress {
+    description = "Minecraft Server Rule #2"
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #tfsec:ignore:aws-vpc-no-public-egress-sgr
   egress {
-    description = "Open Egress for Minecraft Server"
+    description = "Minecraft Server Rule #3"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS009
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = merge(
